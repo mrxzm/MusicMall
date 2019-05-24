812383c0b3c501184284a672ff36d35ec4c98c12
mrxzm Mo <mrxzm@live.com>
2019-05-25 01:04:31 +0800

    用户管理 后台代码

--- MusicMall/Areas/Admin/Controllers/UserController.cs
+++ MusicMall/Areas/Admin/Controllers/UserController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using MusicMall.Areas.Admin.Models;
+using MusicMall.Models;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,16 +12,27 @@ namespace MusicMall.Areas.Admin.Controllers
     public class UserController : BaseController
     {
         // GET: Admin/User
-        public ActionResult Index()
+        public ActionResult Index(int pageNum = 0, int pageSize = 30, string keyword = "")
         {
-            //var users = db.t_admin.Join;
-            return View();
+            IQueryable < t_admin > iq = db.t_admin;
+            if (keyword != "")
+            {
+                keyword = "%" + keyword + "%";
+                iq.Where(w => w.name == keyword);
+            }
+            if (pageNum != 0)
+            {
+                pageNum--;
+            }
+            var users = iq.OrderBy(o => o.id).Skip(pageNum * pageSize).Take(pageSize).ToList();
+            return View(users);
         }
 
         // GET: Admin/User/Details/5
         public ActionResult Details(int id)
         {
-            return View();
+            var user = db.t_admin.Find(id);
+            return View(user);
         }
 
         // GET: Admin/User/Create
@@ -29,61 +43,83 @@ namespace MusicMall.Areas.Admin.Controllers
 
         // POST: Admin/User/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        public ActionResult Create(t_admin admin)
         {
             try
             {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
+                db.t_admin.Add(admin);
+                db.SaveChanges();
+                return Json(new JsonData("ok"));
             }
             catch
             {
-                return View();
+                return Json(new JsonData("no", "创建失败！"));
             }
         }
 
         // GET: Admin/User/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            var user = db.t_admin.Find(id);
+            return View(user);
         }
 
         // POST: Admin/User/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult Edit(t_admin admin)
         {
             try
             {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
+                var data = db.t_admin.Where(w => w.id == admin.id).First();
+                data = Common.Common.MapperToModel<t_admin, t_admin>(admin, data);
+                db.SaveChanges();
+                return Json(new JsonData("ok"));
             }
             catch
             {
-                return View();
+                return Json(new JsonData("no", "创建失败！"));
             }
-        }
-
-        // GET: Admin/User/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
         }
 
         // POST: Admin/User/Delete/5
         [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
+        public ActionResult Delete(int id)
         {
             try
             {
-                // TODO: Add delete logic here
+                var admin = db.t_admin.Find(id);
+                db.t_admin.Remove(admin);
+                db.SaveChanges();
+                return Json(new JsonData("ok"));
+            }
+            catch (Exception)
+            {
 
-                return RedirectToAction("Index");
+                return Json(new JsonData("no", message:"删除失败！"));
+            }
+        }
+
+        // POST: Admin/User/DeleteAll/5/3
+        [HttpPost]
+        public ActionResult DeleteAll(params int[] ids)
+        {
+            try
+            {
+                // 字符串形式 in(1,2,3,4,5) ids == string
+                //var admins = db.t_admin.Where(w => ids.Contains(w.id));
+                //db.t_admin.RemoveRange(admins);
+                
+                foreach (var item in ids)
+                {
+                    var admin = db.t_admin.Find(item);
+                    db.t_admin.Remove(admin);
+                }
+                db.SaveChanges();
+                return Json(new JsonData("ok"));
             }
             catch
             {
-                return View();
+                return Json(new JsonData("no", message: "删除失败！"));
             }
         }
     }
--- MusicMall/Common/Common.cs
+++ MusicMall/Common/Common.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Security.Cryptography;
 using System.Text;
 using System.Web;
@@ -25,5 +26,40 @@ namespace MusicMall.Common
 
             return result;
         }
+
+
+        /// <summary>
+        /// 反射实现两个类的对象之间相同属性的值的复制
+        /// 适用于没有新建实体之间
+        /// </summary>
+        /// <typeparam name="D">返回的实体</typeparam>
+        /// <typeparam name="S">数据源实体</typeparam>
+        /// <param name="d">返回的实体</param>
+        /// <param name="s">数据源实体</param>
+        /// <returns></returns>
+        public static D MapperToModel<D, S>(D d, S s)
+        {
+            try
+            {
+                var Types = s.GetType();//获得类型  
+                var Typed = typeof(D);
+                foreach (PropertyInfo sp in Types.GetProperties())//获得类型的属性字段  
+                {
+                    foreach (PropertyInfo dp in Typed.GetProperties())
+                    {
+                        if (dp.Name == sp.Name && dp.PropertyType == sp.PropertyType && dp.Name != "Error" && dp.Name != "Item")//判断属性名是否相同  
+                        {
+                            dp.SetValue(d, sp.GetValue(s, null), null);//获得s对象属性的值复制给d对象的属性  
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return d;
+        }
     }
+
 }
