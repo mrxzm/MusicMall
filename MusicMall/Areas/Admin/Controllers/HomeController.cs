0273b56fe9ed1a9f0bb0588efd7671c6a7fe389d
mrxzm Mo <mrxzm@live.com>
2019-05-31 01:20:15 +0800

    首页数据

--- MusicMall/Areas/Admin/Controllers/HomeController.cs
+++ MusicMall/Areas/Admin/Controllers/HomeController.cs
@@ -16,6 +16,21 @@ namespace MusicMall.Areas.Admin.Controllers
 
         public ActionResult Welcome()
         {
+            DateTime startTime = DateTime.Now.Date;
+            DateTime endTime = DateTime.Now.Date.AddDays(1);
+            //今日订单数
+            try
+            {
+                int orderCount = db.t_order.Where(w => w.createTime < endTime && w.createTime > startTime).Count();
+                ViewBag.orderCount = orderCount;
+                //今日订单总金额
+                decimal? orderMoney = db.t_order.Where(w => w.createTime < endTime && w.createTime > startTime).Sum(s => s.total);
+                ViewBag.orderMoney = orderMoney == null ? 0 : orderCount;
+            }
+            catch
+            {
+            }
+            
             return View();
         }
     }
